6d2ffa5581a2f29bfe02f550bbe52a7f60cffd20
semcold71 <semcold@gmail.com>
2022-04-15 16:42:07 +0300

    Icon in taskbar (dock)

--- src/main/java/ru/samcold/rtks/RtksApplication.java
+++ src/main/java/ru/samcold/rtks/RtksApplication.java
@@ -4,10 +4,27 @@ import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 @SpringBootApplication
 public class RtksApplication {
 
     public static void main(String[] args) {
+
+        // set icon into dock
+        Taskbar taskbar = Taskbar.getTaskbar();
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(RtksApplication.class.getResource("/images/big-logo.png"));
+            taskbar.setIconImage(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // launch
         Application.launch(FormApplication.class, args);
     }
 }
